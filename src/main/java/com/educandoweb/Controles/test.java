c5b8e93361ea6a9042dba4c615922c2d043ae506
unknown <silvadesousagabriel13@gmail.com>
2024-05-04 13:18:02 -0300

    Criando a tabela no mysql

---
+++ src/main/java/com/educandoweb/Controles/test.java
@@ -0,0 +1,5 @@
+package com.educandoweb.Controles;
+
+public class test {
+    
+}
--- src/main/java/com/educandoweb/curso/Modelos/User.java
+++ src/main/java/com/educandoweb/curso/Modelos/User.java
@@ -3,75 +3,80 @@ package com.educandoweb.curso.Modelos;
 import java.io.Serializable;
 import java.util.UUID;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="tb_user")
 public class User implements Serializable {
 
     private static final long serialVersionUID = 1l;
 
-    private UUID id;
-    private String name;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID idUser;
+    private String nome;
     private String email;
-    private String phone;
-    private String password;
+    private String senha;
+    private int telefone;
 
     public User(){
-        
-    }
 
-    public User(UUID id, String name, String email, String phone, String password) {
-        this.id = id;
-        this.name = name;
+    }
+    
+    
+    public User(UUID idUser, String nome, String email, String senha, int telefone) {
+        this.idUser = idUser;
+        this.nome = nome;
         this.email = email;
-        this.phone = phone;
-        this.password = password;
+        this.senha = senha;
+        this.telefone = telefone;
     }
 
-    public UUID getId() {
-        return id;
-    }
 
-    public void setId(UUID id) {
-        this.id = id;
+    public UUID getIdUser() {
+        return idUser;
     }
-
-    public String getName() {
-        return name;
+    public void setIdUser(UUID idUser) {
+        this.idUser = idUser;
     }
-
-    public void setName(String name) {
-        this.name = name;
+    public String getNome() {
+        return nome;
     }
-
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
-
-    public String getPhone() {
-        return phone;
+    public String getSenha() {
+        return senha;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    public int getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
     }
 
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 
     @Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((idUser == null) ? 0 : idUser.hashCode());
         return result;
     }
+
 
     @Override
     public boolean equals(Object obj) {
@@ -82,14 +87,14 @@ public class User implements Serializable {
         if (getClass() != obj.getClass())
             return false;
         User other = (User) obj;
-        if (id == null) {
-            if (other.id != null)
+        if (idUser == null) {
+            if (other.idUser != null)
                 return false;
-        } else if (!id.equals(other.id))
+        } else if (!idUser.equals(other.idUser))
             return false;
         return true;
     }
-
-    
     
 }
+
+    
